1f651ca721abbbe765745dcf28ced5d658ad6153
mocchan0625 <yuya.mottate0625@gmail.com>
2015-01-31 00:49:14 +0900

    Finish signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130141911) do
+ActiveRecord::Schema.define(version: 20150130153852) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
